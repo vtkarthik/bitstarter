d936c01c102e6940e99fa3cb6e36cb2fe599acc6
karthik vt <vtkarthik@gmail.com>
2013-07-26 19:08:40 +0000

    Modified web.js to pass the requrired argument directly to request.send

--- web.js
+++ web.js
@@ -5,10 +5,13 @@ app.use(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');   
   buf = new Buffer(256);
-var out = fs.readFileSync('./index.html',buf);
-// console.log(buf.toString());
+//var out = 
+//console.log
+response.send(fs.readFileSync('./index.html').toString());
+//buf.write(fs.readFileSync('./index.html'),"utf-8");
+// buf.toString('utf-8')
 //console.log(out.toString());
-response.send(out.toString());
+//response.send(out.toString());
 });
 
 var port = process.env.PORT || 5000;
